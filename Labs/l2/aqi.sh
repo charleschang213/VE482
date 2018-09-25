2b0d583b2bab9e39ce19d860f1f5658a931d98ff
charleschang213 <869048286@qq.com>
2018-09-25 17:22:49 +0800

    l2

---
+++ Labs/l2/aqi.sh
@@ -0,0 +1,1 @@
+curl --silent 'http://aqicn.org/?city=Shanghai&widgetscript&size=large&id=52b39d71decf07.20261781' | sed "s/<[^>]*>//g" | grep 'var content'| sed "s/.*Air Quality.\([0-9]*\).*Temp. \([0-9]*\).*/AQ: \1 Temp: \2/g"
