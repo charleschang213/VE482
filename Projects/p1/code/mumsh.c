d40fc331f277e670bdb625254198f0064969a8cf
charleschang213 <charleschang213@sjtu.edu.cn>
2018-10-06 08:53:46 +0800

    13

--- Projects/p1/code/mumsh.c
+++ Projects/p1/code/mumsh.c
@@ -14,48 +14,62 @@
 #include "cmdl.h"
 #include "funcs.h"
 
+	
 int main(){
 	while (1){
-		char a;
-		clearerr(stdin);
-		fseek(stdin,0,SEEK_SET);
-		setbuf(stdin,NULL);
-		printf("mumsh $ ");
-		fflush(stdout);
-		char command[1024]={0};
-		int flag=0;
-		a = getchar();	
-		if (a=='\n') continue;
-		if (feof(stdin)) {
-			break;
-		}
-		//fflush(stdin);
-		while ((a!='\n')&&(!feof(stdin))){
-			command[flag]=a;
-			flag++;
-			a = getchar();
-			while (a==EOF){
-				clearerr(stdin);
-				a = getchar();
+		pid_t son = fork();
+		if (son==0){
+			char a;
+			clearerr(stdin);
+			fseek(stdin,0,SEEK_SET);
+			setbuf(stdin,NULL);
+			printf("mumsh $ ");
+			fflush(stdout);
+			char command[1024]={0};
+			int flag=0;
+			a = getchar();	
+			if (a=='\n') exit(0);
+			if (feof(stdin)) {
+				exit(1);
 			}
-			fflush(stdin);
-		}
-		const char *esc="exit";
-		const char *cdd = "cd";
-		if (feof(stdin)) break;
-		if (strcmp(command,"\n")==0) continue;
-		//command[strlen(command)-1]=0;
-		if (strcmp(command,esc)==0) break;
-		cmdl line_parsed = parse(command);
-		if (strcmp(line_parsed.commands[0].argv[0],cdd)==0){
-			chdir(line_parsed.commands[0].argv[1]);
+			//fflush(stdin);
+			while ((a!='\n')&&(!feof(stdin))){
+				command[flag]=a;
+				flag++;
+				a = getchar();
+				while (a==EOF){
+					clearerr(stdin);
+					a = getchar();
+				}
+				fflush(stdin);
+			}
+			const char *esc="exit";
+			const char *cdd = "cd";
+			if (feof(stdin)) break;
+			if (strcmp(command,"\n")==0) exit(0);
+			//command[strlen(command)-1]=0;
+			if (strcmp(command,esc)==0) exit(1);
+			cmdl line_parsed = parse(command);
+			if (strcmp(line_parsed.commands[0].argv[0],cdd)==0){
+				chdir(line_parsed.commands[0].argv[1]);
+				cmdl_clean(line_parsed);
+				continue;
+			}
+			pid_t pid = fork();
+			if (pid==0) exec_cmdl(line_parsed);
+			else waitpid(pid,NULL,0);
 			cmdl_clean(line_parsed);
-			continue;
+			exit(0);
 		}
-		pid_t pid = fork();
-		if (pid==0) exec_cmdl(line_parsed);
-		else waitpid(pid,NULL,0);
-		cmdl_clean(line_parsed);
+		else {
+			int *status=malloc(sizeof(int));
+			waitpid(son,status,0);
+			if (*status==256) {
+				free(status);
+				break;
+			}
+			free(status);
+		}
 	}
 	printf("exit\n");
 	return 0;
