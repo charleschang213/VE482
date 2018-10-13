c072f0035118a9c4e87b307e815e459d37b0e63a
charleschang213 <869048286@qq.com>
2018-10-13 20:05:59 +0800

    l2

--- Labs/l2/aqi.sh
+++ Labs/l2/aqi.sh
@@ -1,2 +1,2 @@
 # !/bin/bash
-curl --silent 'http://aqicn.org/?city=Shanghai&widgetscript&size=large&id=52b39d71decf07.20261781' | sed -e "s/<[^>]*>//g" -e "s/.*Air Quality.\([0-9]*\).*Temp. \([0-9]*\).*/AQ: \1 Temp: \2°C/g" -e "s/^[^A].*//g"
+curl --silent 'http://aqicn.org/?city=Shanghai&widgetscript&size=large&id=52b39d71decf07.20261781' | sed -e "s/<[^>]*>//g" -e "s/.*Air Quality.\([0-9]*\).*Temp. \([0-9]*\).*/AQ: \1 Temp: \2 C/g" -e "s/^[^A].*//g"
