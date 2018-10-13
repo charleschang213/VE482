2060ddd0fe1906d61205f0eeb7fdfed114e3ce85
charleschang213 <charleschang213@sjtu.edu.cn>
2018-10-13 09:33:11 +0800

    30.13

--- Projects/p1/code/cmdl.c
+++ Projects/p1/code/cmdl.c
@@ -105,7 +105,6 @@ cmdl parse(char* str,char** quotelist){
 			if (outr){
 				if ((newline.commands[i].ordr)||(i!=newline.cmdc-1)){
 					newline.outdup = 1;
-					newline.commands[i].ordr = malloc(sizeof(char));
 					for (int i=0;i<newline.cmdc;i++) free(command_group[i]);
 					free(command_group);
 					free(ttmp);
