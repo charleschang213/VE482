7720ce0ec8637e809984a29e4ddd525c1c182f9f
charleschang213 <869048286@qq.com>
2018-11-02 01:20:09 +0800

    2.3

--- Projects/p2/src/query/data/DuplicateQuery.cpp
+++ Projects/p2/src/query/data/DuplicateQuery.cpp
@@ -31,8 +31,8 @@ QueryResult::Ptr DuplicateQuery::execute() {
                 while (table[newkey]!=nullptr) newkey+="_copy";
                 vector<Table::ValueType> data;
                 data.reserve(table.field().size());
-                for (size_t i = 0; i < table.field().size(); ++i) {
-                    data.push_back((*it)[i]);
+                for (size_t j = 0; j < table.field().size(); ++j) {
+                    data.push_back((*it)[j]);
                     //data.push_back((*it)[table.field()[i]]);
                 }
                 table.insertByIndex(newkey, move(data));
