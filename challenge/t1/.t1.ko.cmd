c79ddf0e9f6a0d2e01a0451af8fce83a3f3ad88a
Charles Chang <charleschang213@sjtu.edu.cn>
2018-11-12 23:30:39 +0800

    t1_change_kernel

--- challenge/t1/.t1.ko.cmd
+++ challenge/t1/.t1.ko.cmd
@@ -1,1 +1,1 @@
-cmd_/root/VE482/challenge/t1/t1.ko := ld -r -m elf_x86_64 -z max-page-size=0x200000 -T ./scripts/module-common.lds --build-id  -o /root/VE482/challenge/t1/t1.ko /root/VE482/challenge/t1/t1.o /root/VE482/challenge/t1/t1.mod.o ;  true
+cmd_/root/VE482/challenge/t1/t1.ko := ld -r -m elf_x86_64  -z max-page-size=0x200000 -T ./scripts/module-common.lds  --build-id  -o /root/VE482/challenge/t1/t1.ko /root/VE482/challenge/t1/t1.o /root/VE482/challenge/t1/t1.mod.o ;  true
