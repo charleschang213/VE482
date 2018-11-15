6c107ebf555ca0692a68103d249ef8333fe96e0a
charleschang213 <869048286@qq.com>
2018-11-15 09:56:15 +0800

    16

--- Projects/p2/src/query/divs/UpdateDiv.cpp
+++ Projects/p2/src/query/divs/UpdateDiv.cpp
@@ -9,12 +9,6 @@ void DivQuery::UpdateDivExe(){
     auto q = dynamic_cast<UpdateQuery*>(query.get());
     int cnt = 0;
     q->init();
-    if (q->operands[0] == "KEY") {
-            q->keyValue = q->operands[1];
-        } else {
-            q->fieldId = table.getFieldIndex(q->operands[0]);
-            q->fieldValue = (Table::ValueType) strtol(q->operands[1].c_str(), nullptr, 10);
-    }
     auto result = q->initCondition(table);
     if (result.second){
         for (auto it = table.begin()+counter*Partnumber;it!=table.end()&&it<table.begin()+(counter+1)*Partnumber;it++){
