e00ea1e20c9318fa70e36c1e2e665e393078e89b
charleschang213 <869048286@qq.com>
2018-11-16 15:13:25 +0800

    134

--- Projects/p2/src/db/Database.cpp
+++ Projects/p2/src/db/Database.cpp
@@ -397,6 +397,7 @@ Database &Database::getInstance(int threads)
         instance = std::unique_ptr<Database>(new Database);
         if (threads == 0)
             instance->threadnum = std::thread::hardware_concurrency();
+            std::cerr << "Thread Number " << instance->threadnum << std::endl;
         else
             instance->threadnum = threads;
         instance->tasks.reserve(100000);
