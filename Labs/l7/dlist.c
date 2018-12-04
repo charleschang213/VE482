3536f994cdf19daae64bd64b4cce4ec394c453ac
charleschang213 <869048286@qq.com>
2018-12-04 20:58:41 +0800

    rere

--- Labs/l7/dlist.c
+++ Labs/l7/dlist.c
@@ -1,315 +1,355 @@
+//
+// Created by camelboat on 18-12-3.
+//
+
 #include "dlist.h"
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
+#include <time.h>
 
-#define type_t int
-#define LL_INT 0
-#define LL_STRING 1
-#define LL_DOUBLE 2
-#define LL_VALUE_TYPES 3
+typedef struct linked_list list_t;
+typedef struct linked_node node_t;
 
-#define print_t int
-#define LL_INC 1
-#define LL_DEC 2
-#define LL_RAND 0
-#define LL_PRINT_TYPES 3
+struct linked_node
+{
+    char *name;
+    dlistValue value;
+    node_t *next;
+    node_t *before;
+};
 
-typedef struct _node_ {
-	char *key;
-	void *value;
-	struct _node_ *next,*prev;
-}node_t;
+struct linked_list
+{
+    dlistValueType value_type;
+    node_t *first;
+    size_t num;
+};
 
-typedef struct _list_t_ {
-	int size;
-	type_t type;
-	print_t pr;
-	node_t *head,*tail;
-}list_t;
-
-void ll_init(list_t *L,type_t t){
-	L->size = 0;
-	L->type = t;
-	L->head = L->tail = NULL;
-	L->pr = LL_INC;
-}
-void ll_clean(list_t *L){
-	node_t *tmp = L->head;
-	while (tmp){
-		node_t *ttmp = tmp->next;
-		free(tmp->value);
-		free(tmp->key);
-		free(tmp);
-		tmp = ttmp;
-	}
-	ll_init(L,L->type);
+static node_t *createNode(const list_t *list, const char *name, dlistValue value)
+{
+    node_t *newnode = malloc(sizeof(node_t));
+    char *newkey = malloc(strlen(name) + 1);
+    strcpy(newkey, name);
+    newnode->name = newkey;
+    newnode->next = NULL;
+    newnode->before = NULL;
+    if (list->value_type == DLIST_STR)
+    {
+        char *newvalue = malloc(strlen(value.strValue) + 1);
+        strcpy(newvalue, value.strValue);
+        newnode->value.strValue = newvalue;
+    }
+    else
+    {
+        newnode->value = value;
+    }
+    return newnode;
 }
 
-int ll_isempty(list_t *L){
-	return (L->size==0);
+void swap_node(list_t *list, node_t *node_1, node_t *node_2)
+{
+    if (node_1->before == NULL)
+    {
+        list->first = node_2;
+        node_t *tmp = node_2->next;
+        node_2->next = node_1;
+        node_1->before = node_2;
+        if (tmp != NULL)
+        {
+            tmp->before = node_1;
+            node_1->next = tmp;
+        }
+        else
+        {
+            node_1->next = NULL;
+        }
+    }
+    else
+    {
+        node_t *tmp = node_2->next;
+        node_t *tmp_2 = node_1->before;
+        node_2->before = node_1->before;
+        node_1->before = node_2;
+        node_2->next = node_1;
+        tmp_2->next = node_2;
+        if (tmp != NULL)
+        {
+            tmp->before = node_1;
+            node_1->next = tmp;
+        }
+        else
+        {
+            node_1->next = NULL;
+        }
+    }
 }
 
-int int_cmp(void *a,void *b){
-	int *inta = (int*)a;
-	int *intb = (int*)b;
-	int c = *inta-*intb;
-	return (c<0)?-1:((c==0)?0:1);
-}
-int double_cmp(void *a,void *b){
-	double *doublea = (double*)a;
-	double *doubleb = (double*)b;
-	double c = *doublea-*doubleb;
-	return (c<0)?-1:((c==0)?0:1);
-}
-int string_cmp(void *a,void *b){
-	char *chara = (char*)a;
-	char *charb = (char*)b;
-	int c =  strcmp(chara,charb);
-	return (c<0)?-1:((c==0)?0:1);
-}
-int ll_cmp(void *a,void *b,type_t t){
-	int (*cmps[LL_VALUE_TYPES])(void *a,void *b) = {int_cmp,string_cmp,double_cmp};
-	if ((LL_INT<=t)&&(LL_DOUBLE>=t)) return cmps[t](a,b);
-	return -2;
-}
-
-void int_cpy(void **dst,void *src){
-	(*dst) = malloc(sizeof(int));
-	memcpy(*dst,src,sizeof(int));
-}
-void double_cpy(void **dst,void *src){
-	(*dst) = malloc(sizeof(double));
-	memcpy(*dst,src,sizeof(double));
-}
-void string_cpy(void **dst,void *src){
-	int len = strlen(src);
-	(*dst) = malloc((len+1)*sizeof(char));
-	strcpy((char*)(*dst),(char*)(src));
-}
-int value_cpy(void **dst,void *src,type_t t){
-	void (*cpys[LL_VALUE_TYPES])(void **dst,void *src) = {int_cpy,string_cpy,double_cpy};
-	if ((LL_INT<=t)&&(LL_DOUBLE>=t)) {
-		cpys[t](dst,src);
-		return 0;
-	}
-	return -2;
+void sort_list_rand(const list_t *src_list, list_t *dst_list)
+{
+    node_t *tmp = src_list->first;
+    for (int i = 0; i < src_list->num; ++i)
+    {
+        dlistAppend(dst_list, tmp->name, tmp->value);
+        tmp = tmp->next;
+    }
+    for (int i = 0; i < dst_list->num * dst_list->num; ++i)
+    {
+        node_t *tmp = dst_list->first;
+        node_t *tmp_2 = tmp;
+        for (int j = 0; j < dst_list->num-1; ++j)
+        {
+            tmp_2 = tmp->next;
+            if (rand() % 2 == 1)
+            {
+                swap_node(dst_list, tmp, tmp->next);
+            }
+            tmp = tmp_2;
+        }
+    }
 }
 
-
-node_t *ll_search(list_t *L,void *v){
-	node_t *flag = L->head;
-	while ((flag)&&(ll_cmp(v,flag->value,L->type)!=0)) flag = flag->next;
-	return flag;
+void sort_list_inc(const list_t *src_list, list_t *dst_list)
+{
+    node_t *tmp = src_list->first;
+    for (int i = 0; i < src_list->num; ++i)
+    {
+        dlistAppend(dst_list, tmp->name, tmp->value);
+        tmp = tmp->next;
+    }
+    for (int i = 0; i < dst_list->num; ++i)
+    {
+        node_t *tmp = dst_list->first;
+        for (int j = 0; j < dst_list->num-1; ++j)
+        {
+            if (src_list->value_type == DLIST_INT)
+            {
+                if (tmp->value.intValue > tmp->next->value.intValue)
+                {
+                    swap_node(dst_list, tmp, tmp->next);
+                }
+                else
+                {
+                    tmp = tmp->next;
+                }
+            }
+            else if (src_list->value_type == DLIST_DOUBLE)
+            {
+                if (tmp->value.doubleValue > tmp->next->value.doubleValue)
+                {
+                    swap_node(dst_list, tmp, tmp->next);
+                }
+                else
+                {
+                    tmp = tmp->next;
+                }
+            }
+            else if (src_list->value_type == DLIST_STR)
+            {
+                if (strcmp(tmp->value.strValue, tmp->next->value.strValue) > 0)
+                {
+                    swap_node(dst_list, tmp, tmp->next);
+                }
+                else
+                {
+                    tmp = tmp->next;
+                }
+            }
+        }
+    }
 }
 
-void ll_insert(list_t *L,const char *k,void *v){
-	node_t *newnode = malloc(sizeof(node_t));
-	newnode->key = malloc((strlen(k)+1)*sizeof(char));
-	strcpy(newnode->key,k);
-	value_cpy(&(newnode->value),v,L->type);
-	L->size++;
-	if (L->size==1){
-		newnode->prev = newnode->next=NULL;
-		L->head = L->tail = newnode;
-		return;
-	}
-	//node_t *flag = L->head;
-    
-    newnode->prev=L->tail;
-    newnode->next=NULL;
-    L->tail->next=newnode;
-    L->tail = newnode;
-	/*while ((flag)&&(ll_cmp(flag->value,v,L->type)>=0)) flag = flag->next;
-	if (flag==L->head){
-		newnode->prev = NULL;
-		newnode->next=L->head;
-		L->head->prev = newnode;
-		L->head = newnode;
-	}
-	else if (flag==NULL){
-		newnode->prev=L->tail;
-		newnode->next=NULL;
-		L->tail->next=newnode;
-		L->tail = newnode;
-	}
-	else {
-		newnode->prev = flag->prev;
-		newnode->next = flag;
-		flag->prev->next = newnode;
-		flag->prev=newnode;
-	}*/
-	return;
+void sort_list_dec(const list_t *src_list, list_t *dst_list)
+{
+    node_t *tmp = src_list->first;
+    for (int i = 0; i < src_list->num; ++i)
+    {
+        dlistAppend(dst_list, tmp->name, tmp->value);
+        tmp = tmp->next;
+    }
+    for (int i = 0; i < dst_list->num; ++i)
+    {
+        node_t *tmp = dst_list->first;
+        for (int j = 0; j < dst_list->num-1; ++j)
+        {
+            if (src_list->value_type == DLIST_INT)
+            {
+                if (tmp->value.intValue < tmp->next->value.intValue)
+                {
+                    swap_node(dst_list, tmp, tmp->next);
+                }
+                else
+                {
+                    tmp = tmp->next;
+                }
+            }
+            else if (src_list->value_type == DLIST_DOUBLE)
+            {
+                if (tmp->value.doubleValue < tmp->next->value.doubleValue)
+                {
+                    swap_node(dst_list, tmp, tmp->next);
+                }
+                else
+                {
+                    tmp = tmp->next;
+                }
+            }
+            else if (src_list->value_type == DLIST_STR)
+            {
+                if (strcmp(tmp->value.strValue, tmp->next->value.strValue) < 0)
+                {
+                    swap_node(dst_list, tmp, tmp->next);
+                }
+                else
+                {
+                    tmp = tmp->next;
+                }
+            }
+        }
+    }
 }
 
-void ll_insert_ordered(list_t *L,const char *k,void *v,print_t method){
-	node_t *newnode = malloc(sizeof(node_t));
-	newnode->key = malloc((strlen(k)+1)*sizeof(char));
-	strcpy(newnode->key,k);
-	value_cpy(&(newnode->value),v,L->type);
-	L->size++;
-	if (L->size==1){
-		newnode->prev = newnode->next=NULL;
-		L->head = L->tail = newnode;
-		return;
-	}
-	node_t *flag = L->head;
-    if (method==LL_INC)
-	    while ((flag)&&(ll_cmp(flag->value,v,L->type)<=0)) flag = flag->next;
-    else if (method==LL_DEC)
-        while ((flag)&&(ll_cmp(flag->value,v,L->type)>=0)) flag = flag->next;
-    else{
-        int times = rand()%L->size;
-        for (int i=0;i<times;i++) flag = flag->next;
-    } 
-	if (flag==L->head){
-		newnode->prev = NULL;
-		newnode->next=L->head;
-		L->head->prev = newnode;
-		L->head = newnode;
-	}
-	else if (flag==NULL){
-		newnode->prev=L->tail;
-		newnode->next=NULL;
-		L->tail->next=newnode;
-		L->tail = newnode;
-	}
-	else {
-		newnode->prev = flag->prev;
-		newnode->next = flag;
-		flag->prev->next = newnode;
-		flag->prev=newnode;
-	}
-	return;
+dlist createDlist(dlistValueType type)
+{
+    if (type == DLIST_SORT_RAND || type == DLIST_SORT_INC
+    || type == DLIST_SORT_DEC)
+    {
+        list_t *newlist = malloc(sizeof(list_t));
+        node_t *newnode = malloc(sizeof(node_t));
+        newnode->name = NULL;
+        newlist->first = NULL;
+        newlist->num = 0;
+        newlist->value_type = type;
+        return newlist;
+    }
+    else
+    {
+        perror("type error, failed to create new list\n");
+        return NULL;
+    }
 }
 
-
-void ll_remove(list_t *L,int k){
-	node_t *flag = L->head;
-	for (int i=0;i<k;i++) flag = flag->next;
-	printf("%s=",flag->key);
-	switch (L->type){
-		case LL_INT:
-			printf("%d\n",*((int*)flag->value));
-			break;
-		case LL_DOUBLE:
-			printf("%f\n",*((double*)flag->value));
-			break;
-		default:
-			printf("%s\n",(char*)flag->value);
-			break;
-	}
-	L->size--;
-	if (L->size==0){
-		L->head = L->tail = NULL;
-	}
-	else if (flag==L->head){
-		L->head = flag->next;
-		L->head->prev = NULL;
-	}
-	else if (flag==L->tail){
-		L->tail = flag->prev;
-		L->tail->next = NULL;
-	}
-	else {
-		flag->prev->next = flag->next;
-		flag->next->prev = flag->prev;
-	}
-	free(flag->value);
-	free(flag->key);
-	free(flag);
-	return;
+int dlistIsEmpty(dlist_const this)
+{
+    if (((const list_t*) this)->num == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
 }
 
-void ll_print(list_t *L){
-	/*if (L->pr==LL_INC){
-		node_t *flag = L->tail;
-		while (flag){
-			printf("%s=",flag->key);
-			switch (L->type){
-				case LL_INT:
-					printf("%d\n",*((int*)flag->value));
-					break;
-				case LL_DOUBLE:
-					printf("%f\n",*((double*)flag->value));
-					break;
-				default:
-					printf("%s\n",(char*)flag->value);
-					break;
-			}
-			flag = flag->prev;
-		}*/
-	//}
-	//else if (L->pr==LL_DEC){
-		node_t *flag = L->head;
-		while (flag){
-			printf("%s=",flag->key);
-			switch (L->type){
-				case LL_INT:
-					printf("%d\n",*((int*)flag->value));
-					break;
-				case LL_DOUBLE:
-					printf("%f\n",*((double*)flag->value));
-					break;
-				default:
-					printf("%s\n",(char*)flag->value);
-					break;
-			}
-			flag = flag->next;
-		}
-	/*}
-	else {
-		while (!ll_isempty(L)){
-			int randk = rand()%(L->size);
-			ll_remove(L,randk);
-		}
-	}*/
+void dlistAppend(dlist this, const char* key, dlistValue value)
+{
+    node_t *newnode = createNode(this, key, value);
+    list_t *current_list = this;
+    if (current_list->num == 0)
+    {
+        current_list->first = newnode;
+        current_list->first->before = NULL;
+        current_list->num = 1;
+    }
+    else
+    {
+        node_t *tmp = current_list->first;
+         for (int i = 0; i < current_list->num-1; ++i)
+        {
+            tmp = tmp->next;
+        }
+        tmp->next = newnode;
+        newnode->before = tmp;
+        current_list->num++;
+    }
 }
 
+void dlistSort(dlist_const this, dlist listDst, dlistSortMethod method)
+{
+    const list_t *src_list = this;
+    list_t *dst_list = listDst;
+    if (dst_list->num != 0)
+    {
+        node_t *tmp = dst_list->first;
+        node_t *tmp_2;
+        size_t num = dst_list->num;
+        for (int i = 0; i < num; ++i)
+        {
 
-dlist createDlist(dlistValueType type){
-    list_t *newlist = malloc(sizeof(list_t));
-    if (newlist==NULL) return NULL;
-    ll_init(newlist,(type_t)type-1);
-    return (dlist)newlist;
+            tmp_2 = tmp->next;
+
+            free(tmp->name);
+            if (dst_list->value_type == DLIST_STR) {
+                free(tmp->value.strValue);
+            }
+            free(tmp);
+            tmp = tmp_2;
+        }
+        dst_list->num = 0;
+    }
+    dlistValueType type = src_list->value_type;
+    if (type == DLIST_STR || type == DLIST_INT || type == DLIST_DOUBLE)
+    {
+        if (method == DLIST_SORT_INC)
+        {
+            sort_list_inc(src_list, dst_list);
+        }
+        else if (method == DLIST_SORT_DEC)
+        {
+            sort_list_dec(src_list, dst_list);
+        }
+        else if (method == DLIST_SORT_RAND)
+        {
+            sort_list_rand(src_list, dst_list);
+        }
+        else
+        {
+            perror("wrong method\n");
+        }
+    }
+    else
+    {
+        perror("wrong type\n");
+    }
 }
-int dlistIsEmpty(dlist_const dl){
-    list_t *thelist = (list_t*)dl;
-    return ll_isempty(thelist);
+
+void dlistPrint(dlist_const this)
+{
+    const list_t *current_list = this;
+    node_t *tmp = current_list->first;
+    for (int i = 0; i < current_list->num; ++i)
+    {
+        printf("%s=", tmp->name);
+        if (current_list->value_type == DLIST_INT)
+        {
+            printf("%d\n", tmp->value.intValue);
+        }
+        else if (current_list->value_type == DLIST_DOUBLE)
+        {
+            printf("%lf\n", tmp->value.doubleValue);
+        }
+        else if (current_list->value_type == DLIST_STR)
+        {
+            printf("%s\n", tmp->value.strValue);
+        }
+        tmp = tmp->next;
+    }
 }
 
-void dlistAppend(dlist dl, const char *key, dlistValue value){
-    list_t *thelist = (list_t*)dl;
-    switch (thelist->type){
-        case LL_INT:
-            ll_insert(thelist,key,(void*)&value.intValue);
-            break;
-        case LL_DOUBLE:
-            ll_insert(thelist,key,(void*)&value.doubleValue);
-            break;
-        default:
-            ll_insert(thelist,key,(void*)value.strValue);
-            break;
-    }   
-}
-
-
-void dlistSort(dlist_const dl, dlist listDst, dlistSortMethod method){
-    list_t *src = (list_t*)dl;
-    list_t *dst = (list_t*)listDst;
-    ll_clean(dst);
-    dst->pr = (print_t)method;
-    for (node_t *flag=src->head;flag!=NULL;flag = flag->next)
-        ll_insert_ordered(dst,flag->key,flag->value,(print_t)method-1);
-}
-
-void dlistPrint(dlist_const dl){
-    list_t *thelist = (list_t*)dl;
-    ll_print(thelist);
-}
-
-void dlistFree(dlist dl){
-    list_t *thelist = (list_t*)dl;
-    ll_clean(thelist);
-    free(dl);
+void dlistFree(dlist this)
+{
+    list_t *dst_list = this;
+    node_t *tmp = dst_list->first;
+    node_t *tmp_2;
+    size_t num = dst_list->num;
+    for (int i = 0; i < num; ++i)
+    {
+        tmp_2 = tmp->next;
+        if (dst_list->value_type == DLIST_STR) {
+            free(tmp->value.strValue);
+        }
+        tmp = tmp_2;
+        free(tmp);
+    }
+    free(dst_list);
 }
--- Labs/l7/dlist.h
+++ Labs/l7/dlist.h
@@ -1,24 +1,28 @@
-#ifndef _LAB4_DLIST_H_
-#define _LAB4_DLIST_H_
+//
+// Created by camelboat on 18-12-3.
+//
+
+#ifndef LAB7_CORRECT_DLIST_H
+#define LAB7_CORRECT_DLIST_H
 
 // Data type stored in the dlist
-typedef enum dlistValueType_t
-{
-    DLIST_INT = 0x01,    // list contains int values
-    DLIST_STR = 0x02,    // list contains char* values
+typedef enum dlistValueType_t {
+    DLIST_INT    = 0x01, // list contains int values
+    DLIST_STR    = 0x02, // list contains char* values
     DLIST_DOUBLE = 0x03, // list contains double values
     DLIST_UNKOWN = 0x00
 } dlistValueType;
 
+
 // Different options for sorting
 // String are sorted in lexical order
-typedef enum dlistSortMethod_t
-{
-    DLIST_SORT_RAND = 0x01, // Randomize entries
-    DLIST_SORT_INC = 0x02,  // Sort in ascending order
-    DLIST_SORT_DEC = 0x03,  // Sort in descending order
+typedef enum dlistSortMethod_t {
+    DLIST_SORT_RAND  = 0x01, // Randomize entries
+    DLIST_SORT_INC   = 0x02, // Sort in ascending order
+    DLIST_SORT_DEC   = 0x03, // Sort in descending order
     DLIST_SORT_UNKOWN = 0x00
 } dlistSortMethod;
+
 
 // The value stored in the dlist
 // Research online what an "union" is.
@@ -28,43 +32,45 @@ typedef union dlistValue_t {
     char *strValue;
 } dlistValue;
 
+
 typedef void *dlist;
 typedef const void *dlist_const;
 
 dlist createDlist(dlistValueType type);
 // EFFECTS: allocate and create a new dlist of datatype 'type' object
-// returns the created dlist object on success
-// returns NULL on error
+//          returns the created dlist object on success
+//          returns NULL on error
 
-int dlistIsEmpty(dlist_const dl);
-// REQUIRES: argument 'dl' is non-null and is a valid list
-// EFFECTS: return whether the list 'dl' is empty
+int dlistIsEmpty(dlist_const this);
+// REQUIRES: argument 'this' is non-null and is a valid list
+// EFFECTS: return whether the list 'this' is empty
 
-void dlistAppend(dlist dl, const char *key, dlistValue value);
+void dlistAppend(dlist this, const char *key, dlistValue value);
 // REQUIRES: type instantiated in 'value' corresponds to the datatype in the list
-// argument 'dl' and 'key' are both non-null and valid lists
-// EFFECTS : add a line of form "key=value" to the tail of the dlist
-// MODIFIES: modifies 'dl'.
+//            argument 'this' and 'key' are both non-null and valid lists
+// EFFECTS : add a line of form ”key=value” to the tail of the dlist
+// MODIFIES: modifies 'this'.
 
-void dlistSort(dlist_const dl, dlist listDst, dlistSortMethod method);
-// REQUIRES: argument 'dl' is not null and is a valid list
-// argument 'listDst' is not null and is a valid list
+void dlistSort(dlist_const this, dlist listDst, dlistSortMethod method);
+// REQUIRES: argument 'this' is not null and is a valid list
+//            argument 'listDst' is not null and is a valid list
 // EFFECTS: Sort 'listSrc' using method given by 'method'.
-// Put the results in 'listDst' on success
-// Leave 'listDst' unchanged on failure
-// * Note 'listDst' may be non-empty when invoked.
-// * Be very careful with memory management regarding string lists.
+//          Put the results in 'listDst' on success
+//          Leave 'listDst' unchanged on failure
+//          * Note 'listDst' may be non-empty when invoked.
+//          * Be very careful with memory management regarding string lists.
 // MODIFIES: argument 'listDst'
 
-void dlistPrint(dlist_const dl);
-// REQUIRES: argument 'dl' is not null and is a valid list
-// EFFECTS: Print the content of 'dl' in the required format to standard output.
-// A newline must be followed after each line (including the last one).
+void dlistPrint(dlist_const this);
+// REQUIRES: argument 'this' is not null and is a valid list
+// EFFECTS: Print the content of 'this' in the required format to standard output.
+//          A newline must be followed after each line (including the last one).
 
-void dlistFree(dlist dl);
-// REQUIRES: argument 'dl' is <EITHER> NULL or a valid list
-// EFFECTS: if 'dl' is NULL do nothing, other wise frees the list
-// By freeing the list the user also needs to free the data
-// the list manages
+void dlistFree(dlist this);
+// REQUIRES: argument 'this' is ¡EITHER¿ NULL or a valid list
+// EFFECTS: if 'this' is NULL do nothing, other wise frees the list
+//          By freeing the list the user also needs to free the data
+//          the list manages
 
-#endif
+
+#endif //LAB7_CORRECT_DLIST_H
